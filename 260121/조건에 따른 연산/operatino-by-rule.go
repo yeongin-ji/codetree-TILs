b8d266f731393d281ed73f059a29c9695fc577ad
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-21 23:05:52 +0900

    Title: 조건에 따른 연산, Time: 8ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260121/조건에 따른 연산/operatino-by-rule.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, cnt int
+	fmt.Scan(&n)
+	for n >= 1000 {
+		if n%2==0 {
+			n = n*3 + 1
+			cnt++
+		} else {
+			n = n*2 + 2
+			cnt++
+		}
+	}
+	fmt.Print(cnt)
+}
