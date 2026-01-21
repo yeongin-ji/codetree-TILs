cd24c5e1def0c5f6f478695aaba3480fd19f5b72
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-21 15:39:42 +0900

    Title: 청소당번 정하기, Time: 8ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260121/청소당번 정하기/cleaning-numbering.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c2, c3, c12, n int
+	fmt.Scanf("%d", &n)
+	for i := range n {
+		if i == 0 {
+			continue
+		}
+
+		if i%12==0 {
+			c12++
+		} else if i%3==0 {
+			c3++
+		} else if i%2==0 {
+			c2++
+		}
+	}
+	fmt.Print(c2, c3, c12)
+}
