791d74dcf951600efa8336aecf776a4384d1febf
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-21 19:11:33 +0900

    Title: 조건에 부합하는 수, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

--- 260121/조건에 부합하는 수/number-that-meets-the-condition.go
+++ 260121/조건에 부합하는 수/number-that-meets-the-condition.go
@@ -6,7 +6,7 @@ func main() {
 	var a int
 	fmt.Scanf("%d", &a)
 	for i := 1; i <= a; i++ {
-		if (i%2!=0 || i%4==0) && (i/8)%2!=0 && i%7<4 {
+		if (i%2!=0 || i%4==0) && (i/8)%2!=0 && i%7>=4 {
 			fmt.Printf("%d ", i)
 		}
 	}	
