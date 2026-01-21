2dd28e2e0b53cb414f7c69d814362cb0848b0ed6
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-21 14:39:48 +0900

    Title: 숫자의 증감, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260121/숫자의 증감/increasing-and-decreasing-numbers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c rune
+	var n, d int
+	fmt.Scanf("%c %d", &c, &n)
+	switch c {
+	case 'A':
+		for i := 1; i <= n; i++ {
+			fmt.Printf("%d ", i)
+		}
+	case 'D':
+		for i := n; i >= 1; i-- {
+			fmt.Printf("%d ", i)
+		}
+	}	
+}
