23c457751cf932088aa4f57d87cf6f458be49e77
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-22 16:15:47 +0900

    Title: 별표 출력하기 9, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 260122/별표 출력하기 9/print-star-9.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		for j := 1; j < n-i; j++ {
+			fmt.Print("  ")
+		}
+		for j := 1; j <= i*2+1; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}	
+}
