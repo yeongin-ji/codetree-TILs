4443467703093a74f296e1b11dd125823b35c464
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-22 17:11:13 +0900

    Title: 별표 출력하기 6, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

--- 260122/별표 출력하기 6/print-star-6.go
+++ 260122/별표 출력하기 6/print-star-6.go
@@ -6,20 +6,20 @@ func main() {
 	var n int
 	fmt.Scan(&n)
 	for i := range n {
-		for j := range i {
+		for range i {
 			fmt.Print("  ")
 		}
-		for j := range (n-i)*2-1 {
+		for range (n-i)*2-1 {
 			fmt.Print("* ")
 		}
 		fmt.Println()
 	}
 	for i := range n-1 {
-		for j := range n-i-2 {
+		for range n-i-2 {
 			fmt.Print("  ")
 		}
-		for j := range (i+1)*2+1 {
-			fmt.Print(" ")
+		for range (i+1)*2+1 {
+			fmt.Print("* ")
 		}
 		fmt.Println()
 	}
