f71354e0c98ce91801593b23116db74a891cfba9
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-22 16:42:07 +0900

    Title: 두 개의 직각삼각형, Time: 8ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 260122/두 개의 직각삼각형/two-right-triangle.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		for j := 1; j <= n-i; j++ {
+			fmt.Print("*")
+		}
+		for j := 0; j < i*2; j++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= n-i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
