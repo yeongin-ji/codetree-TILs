720213cf5b6d6ce6bcbcdcd68f33b30049185217
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-25 00:12:00 +0900

    Title: 행에 따라 다르게 출력, Time: 10ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260125/행에 따라 다르게 출력/output-defferently-each-row.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	cnt := 0
+	for i := range n {
+		if i%2==0 {
+			for range n {
+				cnt++
+				fmt.Printf("%d ", cnt)
+			}
+		} else {
+			for range n {
+				cnt += 2
+				fmt.Printf("%d", cnt)
+			}
+		}
+		fmt.Println()
+	}	
+}
