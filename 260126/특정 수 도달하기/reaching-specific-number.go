d6571beae704529864b4a90914e2a92667f48755
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-26 22:05:07 +0900

    Title: 특정 수 도달하기, Time: 8ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260126/특정 수 도달하기/reaching-specific-number.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n [10]int
+	sum := 0
+	cnt := 0
+	for i := range 10 {
+		fmt.Scan(&n[i])
+		if n[i] < 250 {
+			sum += n[i]
+			cnt++
+		} else {
+			break
+		}
+	}
+	avg := float64(sum)/float64(cnt)
+	fmt.Print(sum, avg)
+}
