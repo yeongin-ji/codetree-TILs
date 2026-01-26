fd3aaa70cc649536201c2390603e2eb3754636ce
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-27 00:51:10 +0900

    Title: 배열에서 원소의 특징, Time: 9ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260127/배열에서 원소의 특징/characteristics-of-elements-in-an-array.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [10]int
+	for i := range 10 {
+		fmt.Scan(&arr[i])
+	}	
+	for i, v := range arr {
+		if v%3==0 {
+			fmt.Print(arr[i-1])
+		}
+	}
+}
