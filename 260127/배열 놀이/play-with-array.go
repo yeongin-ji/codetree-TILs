a36351693545761814086d62abc40a8bd79e280a
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-27 21:17:55 +0900

    Title: 배열 놀이, Time: 10ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260127/배열 놀이/play-with-array.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N, Q int
+	var arr [110]int
+	fmt.Scan(&N, &Q)
+	for i := range N {
+		fmt.Scan(&arr[i])
+	}
+	for range Q {
+		var cmd int
+		fmt.Scan(&cmd)
+		switch cmd {
+		case 1:
+			var a int
+			fmt.Scan(&a)
+			fmt.Println(arr[a-1])
+		case 2:
+			var b, idx int
+			fmt.Scan(&b)
+			for j := range N {
+				if arr[j]==b {
+					idx = j
+					break
+				}
+			}
+			fmt.Println(idx+1)
+		case 3:
+			var s, e int
+			fmt.Scan(&s, &e)
+			for j := s; j <= e; j++ {
+				fmt.Printf("%d ", arr[j-1])
+			}
+			fmt.Println()
+		}
+	}
+}
