a77a95e487cbaea6e5b643a2a17599aaff97000a
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-27 20:49:19 +0900

    Title: 나눗셈의 나머지, Time: 8ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260127/나눗셈의 나머지/remainder-of-division.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	var cnt [10]int
+	fmt.Scan(&a, &b)
+	for a >= 1 {
+		cnt[a%b]++
+		a /= b
+	}
+	sum := 0
+	for i := range b {
+		sum += cnt[i] * cnt[i]
+	}
+	fmt.Print(sum)
+}
