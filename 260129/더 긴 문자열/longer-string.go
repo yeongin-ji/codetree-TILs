bfbb67081d15fd6d34c53dc444bd6010bec958d7
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-01-29 17:10:09 +0900

    Title: 더 긴 문자열, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

--- 260129/더 긴 문자열/longer-string.go
+++ 260129/더 긴 문자열/longer-string.go
@@ -7,9 +7,9 @@ func main() {
 	fmt.Scan(&s1, &s2)
 	l1, l2 := len(s1), len(s2)
 	if l1 < l2 {
-		fmt.Print(s2, l2)
+		fmt.Print(s2, " ", l2)
 	} else if l1 > l2 {
-		fmt.Print(s1, l1)
+		fmt.Print(s1, " ", l1)
 	} else {
 		fmt.Print("same")
 	}
