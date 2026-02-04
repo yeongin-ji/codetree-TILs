f77d0cb7bc476e4ff5d0f7e34e1d85fbfe52dfe7
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-02-04 11:33:28 +0900

    Title: 양옆 문자 제거하기, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

--- 260204/양옆 문자 제거하기/del-both-side-char.go
+++ 260204/양옆 문자 제거하기/del-both-side-char.go
@@ -8,7 +8,7 @@ func main() {
 	var s string
 	fmt.Scan(&s)
 	r := []rune(s)
-	r = append(r[:2], r[3:]...)
+	r = append(r[:1], r[2:]...)
 	r = append(r[:len(r)-2], r[len(r)-1])
 	s = string(r)
 	fmt.Println(s)	
