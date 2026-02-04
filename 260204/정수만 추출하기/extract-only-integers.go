0e0d6a6c595aed3429f357d3239e1894956a1f86
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-02-04 17:48:01 +0900

    Title: 정수만 추출하기, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

--- 260204/정수만 추출하기/extract-only-integers.go
+++ 260204/정수만 추출하기/extract-only-integers.go
@@ -9,21 +9,43 @@ func main() {
 	var s1, s2 string
 	fmt.Scan(&s1, &s2)
 	var idx1, idx2 int
+	var a, b int
+	flag := true
+	aflag, bflag := false, false
 	for i, v := range s1 {
 		if v<'0' || v>'9' {
 			idx1 = i
+			flag = false
+			break
 		}
 	}	
+	if flag {
+		a, _ = strconv.Atoi(s1)
+		aflag = true
+	}
+	
+	flag = true
 	for i, v := range s2 {
 		if v<'0' || v>'9' {
 			idx2 = i
+			flag = false
+			break
 		}
 	}
-	b1 := s1[:idx1]
-	b2 := s2[:idx2]
-	s1 = string(b1)
-	s2 = string(b2)
-	a, _ := strconv.Atoi(s1)
-	b, _ := strconv.Atoi(s2)
+	if flag {
+		b, _ = strconv.Atoi(s2)
+		bflag = true
+	}
+
+	if !aflag {
+		b1 := s1[:idx1]
+		s1 = string(b1)
+		a, _ = strconv.Atoi(s1)
+	}
+	if !bflag {
+		b2 := s2[:idx2]
+		s2 = string(b2)
+		b, _ = strconv.Atoi(s2)
+	}
 	fmt.Print(a+b)
 }
