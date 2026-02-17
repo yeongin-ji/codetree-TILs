1f0a4a4eb185efc39b9abd569e23e0537fcbe198
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-02-17 23:48:53 +0900

    Title: k번째로 신기한 문자열, Time: 9ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 260217/k번째로 신기한 문자열/kth-special-string.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"slices"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	k, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	t := scanner.Text()
+
+	words := make([]string, n)
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		words[i] = scanner.Text()
+	}
+
+	// Please write your code here.
+	tmp := make([]string, 0)
+	for _, s := range words {
+		r := []rune(s)
+		if t == string(r[:len(t)]) {
+			tmp = append(tmp, s)
+		}
+	}
+	slices.Sort(tmp)
+	for i, s := range tmp {
+		if i+1==k {
+			fmt.Println(s)
+		}
+	}
+}
