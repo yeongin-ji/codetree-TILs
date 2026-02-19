c7555b65829a0e8f7f1cb05e9c1f385536470477
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-02-20 01:32:30 +0900

    Title: 색종이의 총 넓이, Time: 9ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 260220/색종이의 총 넓이/the-total-area-of-colored-paper.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const size = 210
+
+func SetArea(a *[size][size]int, x, y int) {
+	x -= 8
+	for i := x; i < 8; i++ {
+		for j := y; j < 8; j++ {
+			a[i+100][j+100]++
+		}
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	
+	x1 := make([]int, n)
+	y1 := make([]int, n)
+	
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		x1[i], _ = strconv.Atoi(scanner.Text())
+		scanner.Scan()
+		y1[i], _ = strconv.Atoi(scanner.Text())
+	}
+	
+	// Please write your code here.
+	area := [size][size]int{}
+	for i := range n {
+		SetArea(&area, x1[i], y1[i])
+	}
+	cnt := 0
+	for i := range size {
+		for j := range size {
+			if area[i][j] > 0 {
+				cnt++
+			}
+		}
+	}
+	fmt.Print(cnt)
+}
