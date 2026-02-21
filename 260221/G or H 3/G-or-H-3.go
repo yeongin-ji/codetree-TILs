e90bed948d9d77a50a7f2ddae480743642a69268
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-02-21 17:53:56 +0900

    Title: G or H 3, Time: 9ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260221/G or H 3/G-or-H-3.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"math"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	k, _ := strconv.Atoi(scanner.Text())
+	pos := make([]int, n)
+	letter := make([]string, n)
+	maxVal := math.MinInt
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		posVal, _ := strconv.Atoi(scanner.Text())
+		pos[i] = posVal
+		scanner.Scan()
+		letter[i] = scanner.Text()
+		maxVal = max(maxVal, posVal)
+	}
+
+	// Please write your code here.
+	line := make([]string, maxVal+1)
+	for i := range n {
+		line[pos[i]] = letter[i]
+	}
+	maxVal = math.MinInt
+	for i := 1; i <= n-k+1; i++ {
+		sum := 0
+		for j := range k+1 {
+			sum += Score(line[i+j])
+		}
+		maxVal = max(maxVal, sum)
+	}
+	fmt.Print(maxVal)
+}
+
+func Score(s string) int {
+	rst := 0
+	if s == "G" {
+		rst = 1
+	} else if s == "H" {
+		rst = 2
+	}
+	return rst
+}
